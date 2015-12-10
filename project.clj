7cfd6e4cde0c26e455899f77ded603b2dab445f3
José Sánchez Moreno <jose@o2w.es>
2015-12-11 00:20:54 +0100

    Day 10

--- project.clj
+++ project.clj
@@ -3,7 +3,24 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 ;;[org.clojure/math.combinatorics "0.1.1"]
+                 ;;[com.taoensso/timbre "4.1.4"]
+                 ]
   :main ^:skip-aot adventofcode.core
+  :plugins [[lein-cljsbuild "1.1.1-SNAPSHOT"]
+            ]
+
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
+
+                        :compiler {:main adventofcode.day10
+                                   :output-to "target/app.js"
+                                   :output-dir "target/out"
+                                   :target :nodejs
+                                   :optimizations :advanced
+                                   :source-map-timestamp true}}]}
+  )
---
+++ src/adventofcode/day10.clj
@@ -0,0 +1,30 @@
+(ns adventofcode.day10
+  (:require [taoensso.timbre.profiling :refer [profile p]]
+            [clojure.core.reducers :as r :refer [fold]]))
+
+(def input "3113322113")
+
+
+(defn say [[rep num]] (str (count rep) num))
+
+(defn read-aloud [number]
+  (clojure.string/join (map say (re-seq #"(\d)\1*" number))))
+
+(defn repeat-say [input times]
+  (loop [pos 1
+         number input]
+    (println pos)
+    (if (<= pos times)
+      (recur (inc pos) (read-aloud number))
+      (count number))))
+
+
+(defn day10 []
+  (repeat-say input 40))
+
+(defn day10-2 []
+  (repeat-say input 50))
+
+
+(defn main[]
+  (println (day10)))
